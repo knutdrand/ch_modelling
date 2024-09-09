465e629491a5d8c31a694426567c0e179a9049b9
Knut Rand <knutdrand@gmail.com>
2024-09-09 13:00:51 +0200

    update deps

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
 
 requirements = ['typer', 'gluonts', 'plotly', 'pandas', 'matplotlib',
                 'gluonts[torch]',
-                'git+https://github.com/dhis2/chap-core.git@dev']
+                'climate_health @ git+https://github.com/dhis2/chap-core.git@dev']
 
 test_requirements = ['pytest>=3', "hypothesis"]
 
